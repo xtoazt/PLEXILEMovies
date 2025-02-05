98cfd83793d3da25976aa5509b957dfbc5af0dfc
#willo <153154914+willoo0@users.noreply.github.com>
2025-02-05 09:07:25 +0000

    Update 🚀

--- assets/js/watch.js
+++ assets/js/watch.js
@@ -1,25 +1,31 @@
-window.onload = function() {
-  const hashTitle = decodeURIComponent(window.location.hash.substring(1)).replace(/\+/g, " ");
-  let watchedMovies = JSON.parse(localStorage.getItem("watchedMovies")) || [];
-  const movie = watchedMovies.find(m => m.title === hashTitle);
+window.onload = function () {
+    const hashTitle = decodeURIComponent(window.location.hash.substring(1)).replace(/\+/g, " ");
+    let watchedMovies = JSON.parse(localStorage.getItem("watchedMovies")) || [];
+    const movie = watchedMovies.find(m => m.title === hashTitle);
 
-  if (movie) {
-    const iframe = document.createElement("iframe");
-    iframe.src = movie.videoUrl;
-    iframe.width = "1000px";
-    iframe.height = "470px";
-    iframe.frameborder = "0";
-    iframe.allowFullscreen = true;
-    iframe.setAttribute("allow", "fullscreen");
-    iframe.style.border = "1px solid #332424";
-    iframe.style.borderRadius = "12px";
-    iframe.style.boxShadow = "0 4px 15px rgba(0, 0, 0, 0.2)";
-    iframe.style.margin = "20px 0";
-    iframe.style.transition = "transform 0.3s ease, box-shadow 0.3s ease";
-  
-    document.getElementById("videoContainer").appendChild(iframe);
+    if (!movie) {
+        alert("No video selected.");
+        return;
+    }
+
     document.title = `PLEXILE Movies - ${movie.title}`;
-  } else {
-    alert("No video selected.");
-  }
+
+    const iframe = document.getElementById("videoFrame");
+
+    iframe.src = "/loader.html";
+
+    setTimeout(() => {
+        iframe.src = movie.videoUrl;
+    }, 2000);
 };
+
+(function() {
+    const originalOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function(method, url) {
+        if (url.includes("trendlewiggery.top")) {
+            console.log("Blocking VidSrc Ads, YOUR WELCOME -willo", url);
+            return; 
+        }
+        return originalOpen.apply(this, arguments);
+    };
+})();
